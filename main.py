034a7f9f599c5877362fb92cff9b6a91b656307d
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-12 18:01:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.image(image)
 input = open('lrc_iris.pkl', 'rb')
 model = pkl.load(input)
 
-st.header('Choose the size of flower sepal and petal size')
+st.header('Choose the size of sepal and petal size')
 
 sepal_length = st.slider('Sepal length (cm)', 0.0, 20.0, 0.0, 0.1)
 sepal_width = st.slider('Sepal width (cm)', 0.0, 20.0, 0.0, 0.1)
@@ -22,7 +22,7 @@ petal_width = st.slider('Petal width (cm)', 0.0, 20.0, 0.0, 0.1)
 
 if sepal_length == 0 and sepal_width == 0 and petal_length == 0 and petal_width == 0:
     st.header('Result')
-    st.text('Can\'t specify which type of iris flower')
+    st.text('Can\'t specify which type of iris')
 else:
     if st.button('Predict'):
         feature_vector = np.array([sepal_length, sepal_width, petal_length, petal_width])
