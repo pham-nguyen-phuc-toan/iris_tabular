f49363a31e779a999ab4668602563f7e2e8b5b89
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-12 17:17:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import streamlit as st
-from PIL import Image
 import pickle as pkl
 import numpy as np
 
-IMG_SIZE = 227
+class_list = {'0': 'Setosa', '1': 'Versicolor', '2': 'Virginica'}
 
-class_list = {'0': 'NORMAL', '1': 'PNEUMONIA'}
+st.title('Iris classification based on sepal and petal size')
 
-st.title('Pneumonia prediction based on chest X-Ray image')
-
-input = open('lrc_xray.pkl', 'rb')
+input = open('lrc_iris.pkl', 'rb')
 model = pkl.load(input)
 
 st.header('Upload chest X-Ray image')
-uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Test image')
+sepal_length = st.slider('Sepal length (cm)', 0, 20, 0.1)
+sepal_width = st.slider('Sepal width (cm)', 0, 20, 0.1)
+petal_length = st.slider('Petal length (cm)', 0, 20, 0.1)
+petal_width = st.slider('Petal width (cm)', 0, 20, 0.1)
 
+if sepal_length >= 0 and sepal_width >= 0 and petal_length >= 0 and petal_width >= 0:
     if st.button('Predict'):
-        image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
-        feature_vector = np.array(image)
+        feature_vector = np.array([sepal_length, sepal_width, petal_length, petal_width])
         label = str((model.predict(feature_vector))[0])
 
         st.header('Result')
